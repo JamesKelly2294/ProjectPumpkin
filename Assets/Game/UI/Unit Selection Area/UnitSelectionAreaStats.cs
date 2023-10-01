087f6b2aed8bf91b9a47f5c47efa10dcbb174022
Jacob Ingalls <jacobingalls@yahoo.com>
2023-09-30 23:02:05 -0700

    Bar sizing

--- Assets/Game/UI/Unit Selection Area/UnitSelectionAreaStats.cs
+++ Assets/Game/UI/Unit Selection Area/UnitSelectionAreaStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using info.jacobingalls.jamkit;
 
@@ -10,9 +11,14 @@ public class UnitSelectionAreaStats : MonoBehaviour
     public TextMeshProUGUI MovementText;
     public TextMeshProUGUI ManaText;
     public TextMeshProUGUI HealthText;
+
     public ProgressBar MovementBar;
     public ProgressBar ManaBar;
     public ProgressBar HealthBar;
+
+    public LayoutElement MovementPane;
+    public LayoutElement ManaPane;
+    public LayoutElement HealthPane;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +33,21 @@ public class UnitSelectionAreaStats : MonoBehaviour
     }
 
     public void SetEntity(Entity entity) {
+        MovementPane.preferredWidth = Mathf.Min(entity.MaxMovement, Entity.TheoreticalMovementMax) * 20f;
         MovementBar.SetProgress((float)entity.Movement / (float)entity.MaxMovement);
         MovementText.text = "" + entity.Movement + " / " + entity.MaxMovement;
+        
+        if (entity.MaxMana > 0) {
+            ManaPane.gameObject.SetActive(true);
+            ManaPane.preferredWidth = Mathf.Min(entity.MaxMana, Entity.TheoreticalManaMax) * 20f;
+            ManaBar.SetProgress((float)entity.Mana / (float)entity.MaxMana);
+            ManaText.text = "" + entity.Mana + " / " + entity.MaxMana;
+        } else {
+            ManaPane.gameObject.SetActive(false);
+            ManaText.text = "N/A";
+        }
 
-        ManaBar.SetProgress((float)entity.Mana / (float)entity.MaxMana);
-        ManaText.text = "" + entity.Mana + " / " + entity.MaxMana;
-
+        HealthPane.preferredWidth = Mathf.Min(entity.MaxHealth, Entity.TheoreticalHealthMax) * 20f;
         HealthBar.SetProgress((float)entity.Health / (float)entity.MaxHealth);
         HealthText.text = "" + entity.Health + " / " + entity.MaxHealth;
     }
