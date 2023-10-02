d173c8cbfc26bf9b84dcee40923ff1dbcc73172d
Jacob Ingalls <jacobingalls@yahoo.com>
2023-10-02 13:45:20 -0700

    Should show lose screen

--- Assets/Game/Leafy Game Manager/LeafyGameManager.cs
+++ Assets/Game/Leafy Game Manager/LeafyGameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using info.jacobingalls.jamkit;
 
 public class LeafyGameManager : MonoBehaviour
 {
+
+    private TurnManager _turnManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class LeafyGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_turnManager == null) {
+            _turnManager = FindObjectOfType<TurnManager>();
+        }
+
+        // We lost...
+        if (_turnManager.OwnedEntities(Entity.OwnerKind.Player).Count == 0) {
+            Debug.Log("Should show lose screen!");
+            GetComponent<PubSubSender>().Publish("gameManager.showLose");
+        }
     }
 }
