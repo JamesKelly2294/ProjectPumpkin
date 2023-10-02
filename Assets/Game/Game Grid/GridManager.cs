7036834fa125a4778635b1a5e1a55d454bd8f2fc
James Kelly <dxk2294@rit.edu>
2023-10-02 02:47:02 -0700

    Fix broken shit that somehow still worked on my pc??? w/e

--- Assets/Game/Game Grid/GridManager.cs
+++ Assets/Game/Game Grid/GridManager.cs
@@ -62,7 +62,7 @@ public class GridManager : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _pubSubSender = GetComponent<PubSubSender>();
     }
